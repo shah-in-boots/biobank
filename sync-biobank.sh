a59d6fabfd9d27cbea644dd9895f3ea4ed40ea3c
asshah4 <mrshahman@gmail.com>
2019-08-13 21:12:53 -0400

    Update sync-biobank.sh

--- sync-biobank.sh
+++ sync-biobank.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# Add the log files to the raw patient folder on the cluster for analysis
 rsync -arvuz ./raw_patients/ asshah4@hpc5.sph.emory.edu:projects/biobank/raw_patients/ --delete-after
